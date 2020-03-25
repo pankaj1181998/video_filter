dcc9ccfbdff3cd3c9eaf90e164005b4f27f5fa20
Pankaj Sharma10 <pankaj.sharma10@synapse.com>
2020-03-25 22:42:51 +0530

    Directory and Main Edit

---
+++ directory_module.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+
+class Directory
+   def input_directory(self,input_dir : str)
+     p = Path(input_dir)
+	  try:
+         p.mkdir()
+         print("Created Successfully...")
+      except FileExistsError as exc:
+         print(exc)
+
+   def output_directory(self,output_dir : str)
+      p = Path(output_dir)
+      try:
+      	p.mkdir()
+         print("Created Successfully...")
+      except FileExistsError as exc:
+      	print(exc)
---
+++ main.py
@@ -0,0 +1,12 @@
+import directory_module
+
+if __name__ == '__main__':
+   input_directory_name = "input"
+   output_directory_name = "output"
+   dir = Directory()
+
+   dir.input_directory(input_directory_name)
+   dir.outut_directory(output_directory_name)
+
+   
+
