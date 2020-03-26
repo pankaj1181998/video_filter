efdffbf2f19d38b48de696451fd8fa94330c7f7c
Pankaj Sharma10 <pankaj.sharma10@synapse.com>
2020-03-26 14:06:35 +0530

    adding frames

--- frames.py
+++ frames.py
@@ -1,3 +1,31 @@
 # Importing all necessary libraries 
 import cv2 
 import os
+
+cam = cv2.VideoCapture('input/')
+try :
+   if not os.path.exists('frames'):
+      os.makedirs('frames/')
+except OSError :
+   print ('Error: Creating directory of data')
+
+#frame
+currentframe = 0
+while(True):
+   ret,frame = cam.read()
+
+   if ret:
+      name = 'frames/file_name'+str(currentframe)+'.jpg'
+      print ('Creating...' + name) 
+
+      # Creating Images 
+      cv2.imwrite(name,frame)
+
+      currentframe += 1
+   else:
+      break 
+
+# Release all space and windows once done
+cam.release()
+cv2.destroyAllWindows()
+
