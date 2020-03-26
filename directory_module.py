9c1d3c68e7dd79d67834b7cdc334ef8a1f3fbf10
Pankaj Sharma10 <pankaj.sharma10@synapse.com>
2020-03-26 12:10:12 +0530

    directory.py fixed

--- directory_module.py
+++ directory_module.py
@@ -1,18 +1,22 @@
 from pathlib import Path
 
-class Directory
-   def input_directory(self,input_dir : str)
-     p = Path(input_dir)
-	  try:
+class Directory :
+
+   def __init__(self) :
+      print("Directory Class Initiated")
+
+   def input_directory(self,input_dir: str):
+      p = Path(input_dir)
+      try:
          p.mkdir()
          print("Created Successfully...")
       except FileExistsError as exc:
          print(exc)
 
-   def output_directory(self,output_dir : str)
+   def output_directory(self,output_dir : str):
       p = Path(output_dir)
       try:
-      	p.mkdir()
+         p.mkdir()
          print("Created Successfully...")
       except FileExistsError as exc:
-      	print(exc)
+         print(exc)
--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-import directory_module
+from directory_module import *
 
 if __name__ == '__main__':
+   
    input_directory_name = "input"
    output_directory_name = "output"
+   
    dir = Directory()
-
    dir.input_directory(input_directory_name)
-   dir.outut_directory(output_directory_name)
+   dir.output_directory(output_directory_name)
+
 
    
 
