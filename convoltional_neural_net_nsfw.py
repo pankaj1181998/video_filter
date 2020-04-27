969a63d7cfd7dea057bab38351e5bb3db7684a53
Pankaj Sharma10 <pankaj.sharma10@synapse.com>
2020-04-27 13:02:37 +0530

    CNN Network

--- convoltional_neural_net_nsfw.py
+++ convoltional_neural_net_nsfw.py
@@ -9,6 +9,10 @@ import numpy as np
 import random
 from tensorflow.keras.preprocessing.image import img_to_array, load_img
 
+
+'''
+    Creating Directiories variables ------------------------------
+'''
 base_dir = '/data'
 train_dir = os.path.join(base_dir, 'train')
 validation_dir = os.path.join(base_dir, 'validation')
@@ -33,21 +37,59 @@ sfw_fnames = os.listdir(sfw_dir)
 sfw_fnames.sort()
 print(sfw_fnames[:10])
 
-# Adding rescale, rotation_range, width_shift_range, height_shift_range,
-# shear_range, zoom_range, and horizontal flip to our ImageDataGenerator
-train_datagen = ImageDataGenerator(
-    rescale=1./255,
-    rotation_range=40,
-    width_shift_range=0.2,
-    height_shift_range=0.2,
-    shear_range=0.2,
-    zoom_range=0.2,
-    horizontal_flip=True,)
 
-# Note that the validation data should not be augmented!
+'''
+Building Small Covnet--------------------------------------------------
+'''
+# Our input feature map is 150x150x3: 150x150 for the image pixels, and 3 for
+# the three color channels: R, G, and B
+img_input = layers.Input(shape=(150, 150, 3))
+
+# First convolution extracts 16 filters that are 3x3
+# Convolution is followed by max-pooling layer with a 2x2 window
+x = layers.Conv2D(16, 3, activation='relu')(img_input)
+x = layers.MaxPooling2D(2)(x)
+
+# Second convolution extracts 32 filters that are 3x3
+# Convolution is followed by max-pooling layer with a 2x2 window
+x = layers.Conv2D(32, 3, activation='relu')(x)
+x = layers.MaxPooling2D(2)(x)
+
+# Third convolution extracts 64 filters that are 3x3
+# Convolution is followed by max-pooling layer with a 2x2 window
+x = layers.Conv2D(64, 3, activation='relu')(x)
+x = layers.MaxPooling2D(2)(x)
+
+# Flatten feature map to a 1-dim tensor so we can add fully connected layers
+x = layers.Flatten()(x)
+
+# Create a fully connected layer with ReLU activation and 512 hidden units
+x = layers.Dense(512, activation='relu')(x)
+
+# Create output layer with a single node and sigmoid activation
+output = layers.Dense(1, activation='sigmoid')(x)
+
+# Create model:
+# input = input feature map
+# output = input feature map + stacked convolution/maxpooling layers + fully 
+# connected layer + sigmoid output layer
+model = Model(img_input, output)
+
+model.summary()
+
+# using the RMSprop optimization algorithm
+model.compile(loss='binary_crossentropy',
+              optimizer=RMSprop(lr=0.001),
+              metrics=['acc'])
+
+'''
+    Data Prerocessing-------------------------------------------
+'''
+# All images will be rescaled by 1./255
+train_datagen = ImageDataGenerator(rescale=1./255)
 val_datagen = ImageDataGenerator(rescale=1./255)
 
-# Flow training images in batches of 32 using train_datagen generator
+# Flow training images in batches of 20 using train_datagen generator
 train_generator = train_datagen.flow_from_directory(
         train_dir,  # This is the source directory for training images
         target_size=(150, 150),  # All images will be resized to 150x150
@@ -55,9 +97,20 @@ train_generator = train_datagen.flow_from_directory(
         # Since we use binary_crossentropy loss, we need binary labels
         class_mode='binary')
 
-# Flow validation images in batches of 32 using val_datagen generator
+# Flow validation images in batches of 20 using val_datagen generator
 validation_generator = val_datagen.flow_from_directory(
         validation_dir,
         target_size=(150, 150),
         batch_size=20,
         class_mode='binary')
+
+'''
+    Training Data --------------------------------------------------
+'''
+history = model.fit_generator(
+      train_generator,
+      steps_per_epoch=100,  # 2000 images = batch_size * steps
+      epochs=15,
+      validation_data=validation_generator,
+      validation_steps=50,  # 1000 images = batch_size * steps
+      verbose=2)
