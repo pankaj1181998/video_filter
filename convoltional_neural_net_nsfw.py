c14edde5530793c7ad92241624d421e23749199b
Pankaj Sharma10 <pankaj.sharma10@synapse.com>
2020-04-27 12:53:00 +0530

    convolutional network

--- convoltional_neural_net_nsfw.py
+++ convoltional_neural_net_nsfw.py
@@ -1,5 +1,4 @@
 import os
-import zipfile
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 from tensorflow.keras import layers
@@ -9,3 +8,56 @@ from tensorflow.keras.preprocessing.image import ImageDataGenerator
 import numpy as np
 import random
 from tensorflow.keras.preprocessing.image import img_to_array, load_img
+
+base_dir = '/data'
+train_dir = os.path.join(base_dir, 'train')
+validation_dir = os.path.join(base_dir, 'validation')
+
+# Directory with our training pictures nsfw
+nsfw_dir = os.path.join(train_dir, 'nsfw')
+
+# Directory with our training dog pictures
+sfw_dir = os.path.join(train_dir, 'sfw')
+
+
+# Directory with our validation pictures sfw
+validation_cats_dir = os.path.join(validation_dir, 'nsfw')
+
+# Directory with our validation pictures sfw
+validation_dogs_dir = os.path.join(validation_dir, 'sfw')
+
+nsfw_fnames = os.listdir(nsfw_dir)
+print(nsfw_fnames[:10])
+
+sfw_fnames = os.listdir(sfw_dir)
+sfw_fnames.sort()
+print(sfw_fnames[:10])
+
+# Adding rescale, rotation_range, width_shift_range, height_shift_range,
+# shear_range, zoom_range, and horizontal flip to our ImageDataGenerator
+train_datagen = ImageDataGenerator(
+    rescale=1./255,
+    rotation_range=40,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True,)
+
+# Note that the validation data should not be augmented!
+val_datagen = ImageDataGenerator(rescale=1./255)
+
+# Flow training images in batches of 32 using train_datagen generator
+train_generator = train_datagen.flow_from_directory(
+        train_dir,  # This is the source directory for training images
+        target_size=(150, 150),  # All images will be resized to 150x150
+        batch_size=20,
+        # Since we use binary_crossentropy loss, we need binary labels
+        class_mode='binary')
+
+# Flow validation images in batches of 32 using val_datagen generator
+validation_generator = val_datagen.flow_from_directory(
+        validation_dir,
+        target_size=(150, 150),
+        batch_size=20,
+        class_mode='binary')
