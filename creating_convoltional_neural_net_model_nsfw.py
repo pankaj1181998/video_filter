af6498d86eafec317ebfbb32839423f12de3b7f7
Pankaj Sharma10 <pankaj.sharma10@synapse.com>
2020-04-29 03:23:03 +0530

    model in h5 format

---
+++ creating_convoltional_neural_net_model_nsfw.py
@@ -0,0 +1,146 @@
+import os
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+from tensorflow.keras import layers
+from tensorflow.keras import Model
+from tensorflow.keras.optimizers import RMSprop
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import numpy as np
+import random
+from tensorflow.keras.preprocessing.image import img_to_array, load_img
+
+
+'''
+    Creating Directiories variables -----------------------------------------
+'''
+base_dir = 'D:/Video_filtering/data'
+train_dir = os.path.join(base_dir, 'train')
+validation_dir = os.path.join(base_dir, 'validation')
+
+# Directory with our training pictures nsfw
+nsfw_dir = os.path.join(train_dir, 'nsfw')
+    
+# Directory with our training dog pictures
+sfw_dir = os.path.join(train_dir, 'sfw')
+
+
+# Directory with our validation pictures sfw
+validation_cats_dir = os.path.join(validation_dir, 'nsfw')
+
+# Directory with our validation pictures sfw
+validation_dogs_dir = os.path.join(validation_dir, 'sfw')
+
+nsfw_fnames = os.listdir(nsfw_dir)
+print(nsfw_fnames[:10])
+
+sfw_fnames = os.listdir(sfw_dir)
+sfw_fnames.sort()
+print(sfw_fnames[:10])
+
+
+'''
+    Building Small Covnet----------------------------------------------------
+'''
+# Our input feature map is 150x150x3: 150x150 for the image pixels, and 3 for
+# the three color channels: R, G, and B
+img_input = layers.Input(shape=(150, 150, 3))
+
+# First convolution extracts 16 filters that are 3x3
+# Convolution is followed by max-pooling layer with a 2x2 window
+x = layers.Conv2D(16, 3, activation='relu')(img_input)
+x = layers.MaxPooling2D(2)(x)
+
+# Second convolution extracts 32 filters that are 3x3
+# Convolution is followed by max-pooling layer with a 2x2 window
+x = layers.Conv2D(32, 3, activation='relu')(x)
+x = layers.MaxPooling2D(2)(x)
+
+# Third convolution extracts 64 filters that are 3x3
+# Convolution is followed by max-pooling layer with a 2x2 window
+x = layers.Conv2D(64, 3, activation='relu')(x)
+x = layers.MaxPooling2D(2)(x)
+
+# Flatten feature map to a 1-dim tensor so we can add fully connected layers
+x = layers.Flatten()(x)
+
+# Create a fully connected layer with ReLU activation and 512 hidden units
+x = layers.Dense(512, activation='relu')(x)
+
+# Create output layer with a single node and sigmoid activation
+output = layers.Dense(1, activation='sigmoid')(x)
+
+# Create model:
+# input = input feature map
+# output = input feature map + stacked convolution/maxpooling layers + fully 
+# connected layer + sigmoid output layer
+model = Model(img_input, output)
+
+#Saving model 
+model.save('nsfw.h5')
+
+model.summary()
+
+# using the RMSprop optimization algorithm
+model.compile(loss='binary_crossentropy',
+              optimizer=RMSprop(lr=0.001),
+              metrics=['acc'])
+
+'''
+    Data Preprocessing-----------------------------------------------------
+'''
+# All images will be rescaled by 1./255
+train_datagen = ImageDataGenerator(rescale=1./255)
+val_datagen = ImageDataGenerator(rescale=1./255)
+
+# Flow training images in batches of 20 using train_datagen generator
+train_generator = train_datagen.flow_from_directory(
+        train_dir,  # This is the source directory for training images
+        target_size=(150, 150),  # All images will be resized to 150x150
+        batch_size=20,
+        # Since we use binary_crossentropy loss, we need binary labels
+        class_mode='binary')
+
+# Flow validation images in batches of 20 using val_datagen generator
+validation_generator = val_datagen.flow_from_directory(
+        validation_dir,
+        target_size=(150, 150),
+        batch_size=20,
+        class_mode='binary')
+
+'''
+    Training Data ---------------------------------------------------------
+'''
+history = model.fit_generator(
+      train_generator,
+      steps_per_epoch=100,  # 2000 images = batch_size * steps
+      epochs=15,
+      validation_data=validation_generator,
+      validation_steps=50,  # 1000 images = batch_size * steps
+      verbose=2)
+'''
+    Evaluating Accuracy and Loss for Model---------------------------------
+'''
+# Retrieve a list of accuracy results on training and validation data
+# sets for each training epoch
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+
+# Retrieve a list of list results on training and validation data
+# sets for each training epoch
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+# Get number of epochs
+epochs = range(len(acc))
+
+# Plot training and validation accuracy per epoch
+plt.plot(epochs, acc)
+plt.plot(epochs, val_acc)
+plt.title('Training and validation accuracy')
+
+plt.figure()
+
+# Plot training and validation loss per epoch
+plt.plot(epochs, loss)
+plt.plot(epochs, val_loss)
+plt.title('Training and validation loss')
